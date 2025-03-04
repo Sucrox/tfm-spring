5528577d934f7207f0986613367e03154478e5fa
Adrian Alonso <Adrianalonso2001@gmail.com>
2025-03-04 14:12:10 +0100

    Inicialización del proyecto Sucrox/TFM#6

---
+++ src/main/java/es/upm/miw/companyds/tfm_spring/TfmSpringApplication.java
@@ -0,0 +1,13 @@
+package es.upm.miw.companyds.tfm_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfmSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfmSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/miw/companyds/tfm_spring/TfmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.miw.companyds.tfm_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfmSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
