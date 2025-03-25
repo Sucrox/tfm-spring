65b4b415132cb6aec967cdb659d0bea4c701e2e5
Adrian Alonso <Adrianalonso2001@gmail.com>
2025-03-25 16:43:30 +0100

    Endpoints Sucrox/TFM#13

--- src/main/java/es/upm/miw/companyds/tfm_spring/services/impl/UserServiceImpl.java
+++ src/main/java/es/upm/miw/companyds/tfm_spring/services/impl/UserServiceImpl.java
@@ -13,8 +13,13 @@ import es.upm.miw.companyds.tfm_spring.services.exceptions.NotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -49,11 +54,39 @@ public class UserServiceImpl implements UserService {
     @Override
     public String login(LoginDto loginDto) throws ConflictException {
         User user =  this.userRepository.findByEmail(loginDto.getEmail())
-                    .orElseThrow(() -> new NotFoundException("Unauthorized login"));
+                .orElseThrow(() -> new NotFoundException("Unauthorized login"));
         if (!passwordEncoder.matches(loginDto.getPassword(), user.getPassword())) {
             throw new ForbiddenException("Incorrect fields");
         }
         return  jwtService.createToken(user.getPhone(), user.getFirstName(), user.getRole().name());
+    }
+
+    @Override
+    public Stream<UserDto> getAllUsers(Role role) {
+        if (role.equals(Role.ADMIN)) {
+            return userRepository.findAll().stream().map(UserDto::ofUser);
+        } else {
+            throw new ForbiddenException("You are not allowed to make this call");
+        }
+    }
+
+    @Override
+    public UserDto getUserById(Integer id, Role role) {
+        if(!Objects.equals(id, this.extractUserID()) && !role.equals(Role.ADMIN)) {
+            throw new ForbiddenException("You are not allowed to make this call");
+        }
+        return userRepository.findById(id).map(UserDto::ofUser)
+                .orElseThrow(() -> new NotFoundException("There's no user for id:" + id));
+
+    }
+    private Integer extractUserID() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("No authenticated user found");
+        }
+        String phone = authentication.getName();
+        return userRepository.findByPhone(phone).map(User::getId)
+                .orElseThrow(() -> new ForbiddenException("Authenticated user not found"));
     }
 
 }
